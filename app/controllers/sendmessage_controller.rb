f90d77f557128ef1b2444afbc34ae44207a443bc
Maaz Haque <maazhaque@Maazs-MacBook-Pro.local>
2017-02-26 17:48:00 -0500

    added send message controller to handle send messages

---
+++ app/controllers/sendmessage_controller.rb
@@ -0,0 +1,23 @@
+class SendmessageController < ApplicationController
+
+  def send
+    require 'fcm'
+
+    fcm = FCM.new("AIzaSyBzy7OT-e4qk8sJ5TnMw4kvhPpKjh7i-S4")
+    # you can set option parameters in here
+    #  - all options are pass to HTTParty method arguments
+    #  - ref: https://github.com/jnunemaker/httparty/blob/master/lib/httparty.rb#L29-L60
+    #  fcm = FCM.new("my_api_key", timeout: 3)
+
+    # registration_ids= ["12", "13"] # an array of one or more client registration tokens
+    options =  params[:data]
+
+    # options = {data: {score: "123"}, collapse_key: "updated_score"}
+    response = fcm.send_with_notification_key("/topics/testMessage", data: options)
+    # response = fcm.send(registration_ids,options)
+    render json: response , status: 200
+  end
+
+  
+
+end
--- config/routes.rb
+++ config/routes.rb
@@ -4,5 +4,9 @@ Rails.application.routes.draw do
   get 'messaging/homepage'
   root 'messaging#homepage'
   resources :fire
+
+  namespace :api, :defaults => {:format => :json} do
+    post 'sendmessage', to: "sendmessage#send"
+  end
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
